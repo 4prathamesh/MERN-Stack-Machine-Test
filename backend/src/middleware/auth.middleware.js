174c57c8610f81a1309e94f6b7a9f3353a3a9dab
4prathamesh <wprathamesh2@gmail.com>
2026-03-27 23:55:31 +0530

    add the auth middleware

---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+module.exports = (req, res, err) => {
+    try{
+        const authHeader = req.headers.authorization;
+
+        if(!authHeader)
+            return res.status(401).json({ message: "Unauthorized: No token provided" });
+
+        const token = authHeader.split(' ')[1];
+
+        const decode = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+
+        req.user = decode;
+
+        next();
+    }catch(err){
+        return res.status(401).json({
+            message: "Unauthorized: Invalid or expired token"
+        });
+    }
+
+};
